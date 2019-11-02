43107c631917edcd3cf3a10b45dec6e74dcdfd43
RachmanForniandi <rachmanforniandi@gmail.com>
2019-11-02 18:53:10 +0700

    update delete w/ backend successfull

--- app/src/main/java/com/example/android/personalkasappv2/EditActivity.java
+++ app/src/main/java/com/example/android/personalkasappv2/EditActivity.java
@@ -15,7 +15,15 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.andexert.library.RippleView;
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.example.android.personalkasappv2.dbHelper.Config;
 import com.example.android.personalkasappv2.dbHelper.SqliteHelper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -53,6 +61,15 @@ public class EditActivity extends AppCompatActivity {
 
         et_tanggal= (EditText)findViewById(R.id.et_tanggal);
 
+        editOnMysql();
+        //editOnSQLite();
+        //set title
+        getSupportActionBar().setTitle("Edit Data");
+        //return to Home
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void editOnSQLite(){
         sqliteHelper = new SqliteHelper(this);
 
         SQLiteDatabase database = sqliteHelper.getReadableDatabase();
@@ -133,11 +150,105 @@ public class EditActivity extends AppCompatActivity {
                 }
             }
         });
+    }
 
-        //set title
-        getSupportActionBar().setTitle("Edit Data");
-        //return to Home
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    private void editOnMysql(){
+
+        status = M.status;
+        switch (status){
+            case "MASUK": radio_income.setChecked(true);
+                break;
+            case "KELUAR": radio_outcome.setChecked(true);
+                break;
+        }
+
+        radio_status.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                switch (checkedId){
+                    case R.id.radio_income:
+                        status= "MASUK";
+                        break;
+                    case R.id.radio_outcome:
+                        status= "KELUAR";
+                        break;
+                }
+                Log.d("Log Status", status);
+            }
+        });
+
+        et_jumlah.setText( M.jumlah);
+        et_keterangan.setText( M.keterangan);
+
+        tanggal = M.tanggal2;
+        et_tanggal.setText(M.tanggal);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+        et_tanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                datePickerDialog = new DatePickerDialog(EditActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        NumberFormat formatNumber = new DecimalFormat("00");
+                        tanggal= formatNumber.format(year)+ "/" + formatNumber.format(month + 1)+ "/" +formatNumber.format(dayOfMonth);
+
+                        et_tanggal.setText(formatNumber.format(dayOfMonth) + "/" + formatNumber.format(month + 1) + "/" +
+                                formatNumber.format(year));
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        rip_simpan.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+            @Override
+            public void onComplete(RippleView rippleView) {
+                if(status.equals("") || et_jumlah.getText().toString().equals("") || et_keterangan.getText().toString().equals("")){
+                    Toast.makeText(EditActivity.this, "Isi data dengan benar",
+                            Toast.LENGTH_LONG).show();
+                }else{
+                    AndroidNetworking.post(Config.HOST+"update.php")
+                            .addBodyParameter("transaksi_id", M.transaksi_id)
+                            .addBodyParameter("status", status)
+                            .addBodyParameter("jumlah", et_jumlah.getText().toString())
+                            .addBodyParameter("keterangan", et_keterangan.getText().toString())
+                            .addBodyParameter("tanggal", tanggal)
+                            .setPriority(Priority.MEDIUM)
+                            .build()
+                            .getAsJSONObject(new JSONObjectRequestListener() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    // do anything with response
+                                    try {
+                                        if (response.getString("response").equals("success")){
+                                            //startActivity(new Intent(AddActivity.this,MainActivity.class));
+                                            Toast.makeText(EditActivity.this, "Perubahan data transaksi berhasil disimpan",
+                                                    Toast.LENGTH_LONG).show();
+                                            finish();
+                                        }else {
+                                            Toast.makeText(EditActivity.this, response.getString("response"),
+                                                    Toast.LENGTH_LONG).show();
+                                        }
+                                    }catch (JSONException e){
+                                        e.printStackTrace();
+                                    }
+                                }
+                                @Override
+                                public void onError(ANError error) {
+                                    // handle error
+                                    error.printStackTrace();
+                                }
+                            });
+                }
+            }
+        });
     }
     //ketika icon back diclick
     @Override
